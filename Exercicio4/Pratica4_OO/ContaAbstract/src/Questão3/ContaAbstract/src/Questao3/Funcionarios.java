9ddbf48b5cad4c57b8b992d9534330e51151703b
acannee <57539101+acannee@users.noreply.github.com>
2022-05-05 22:58:07 -0300

    Add files via upload

---
+++ Exercicio4/Pratica4_OO/ContaAbstract/src/Questão3/ContaAbstract/src/Questao3/Funcionarios.java
@@ -0,0 +1,32 @@
+package Questao3;
+
+public class Funcionarios {
+		public String nome;
+		private String RG;
+	
+	public Funcionarios(String nome, String rG) {
+		super();
+		this.nome = nome;
+		RG = rG;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRG() {
+		return RG;
+	}
+
+	public void setRG(String rG) {
+		RG = rG;
+	}
+	
+	
+	
+
+}
